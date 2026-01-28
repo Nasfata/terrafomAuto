891785de2614bcf581ac27f5ca12bf4be7f33c25
Nafsata <nasfatamohamed323@gmail.com>
2026-01-28 18:07:13 +0100

    Ajout du workflow CI/CD

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket        = "terraform-state-mohamed"
-    key           = "terraform.tfstate"
-    region        = "us-east-1"
-    use_lockfile  = true
+    bucket = "terraform-state-mohamed"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
 }
